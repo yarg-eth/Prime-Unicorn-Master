d371202c32281f98bd32aad88e99274370694bc3
yarg-eth <gblanchard679@gmail.com>
2022-06-07 23:29:01 -0500

    revert

--- contracts/PrimeUnicorn.sol
+++ contracts/PrimeUnicorn.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/openzeppelin-contracts/contracts/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
