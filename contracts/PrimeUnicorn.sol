3e581f2a888d8078b5e266a5f5752f0037a24c14
Gray Blanchard <gblanchard679@gmail.com>
2022-07-12 14:08:09 -0500

    quantity fix

--- contracts/PrimeUnicorn.sol
+++ contracts/PrimeUnicorn.sol
@@ -15,7 +15,7 @@ contract PrimeUnicorn is ERC721, Ownable, ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenSupply;
 
-    uint256 public constant MAX_SUPPLY = 10000;
+    uint256 public constant MAX_SUPPLY = 5555;
     uint256 public maxWhitelistMint = 1;
     uint256 public maxMint = 5;
 
@@ -67,6 +67,24 @@ contract PrimeUnicorn is ERC721, Ownable, ReentrancyGuard {
 
     function _baseURI() internal view override returns (string memory) {
         return baseURI;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, tokenId.toString()))
+                : "";
     }
 
     // Minting
