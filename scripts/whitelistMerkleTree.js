cbf0eab416092b1abd4eab28a64c90f928763638
Gray Blanchard <gblanchard679@gmail.com>
2022-08-04 14:29:01 -0500

    new address

--- scripts/whitelistMerkleTree.js
+++ scripts/whitelistMerkleTree.js
@@ -1,7 +1,7 @@
 const { MerkleTree } = require('merkletreejs');
 const keccak256 = require('keccak256');
 
-let whitelistAddresses = ["0x16DD346Aa1483264DBb0Dde64235081C867fb3f2", "0x72672236A8B47f3d84248890FF48662a6d197833", "0x46340b20830761efd32832A74d7169B29FEB9758", "0x91bFc391fDD6E07f54283bbd5F1417c58E872BAD", "0xb2e7e393E8C6Dfe9c311ce786e1E68459253839c", "0x74Fac8b17237e00724E06d20115b7ecFA3389281", "0xE3dd70Ba465735E6C1cddbD3EC4C7eE8aD77B0d8", "0x6d6257976bd82720A63fb1022cC68B6eE7c1c2B0"]
+let whitelistAddresses = ["0x16DD346Aa1483264DBb0Dde64235081C867fb3f2", "0x72672236A8B47f3d84248890FF48662a6d197833", "0x46340b20830761efd32832A74d7169B29FEB9758", "0x91bFc391fDD6E07f54283bbd5F1417c58E872BAD", "0xb2e7e393E8C6Dfe9c311ce786e1E68459253839c", "0x74Fac8b17237e00724E06d20115b7ecFA3389281", "0xE3dd70Ba465735E6C1cddbD3EC4C7eE8aD77B0d8", "0x6d6257976bd82720A63fb1022cC68B6eE7c1c2B0", "0xbea9938565B830a263162C86De7642177fA08004"]
 
 // Merkle Tree & Root Hash
 const leafNodes = whitelistAddresses.map(addr => keccak256(addr));
@@ -15,7 +15,7 @@ console.log('Whitelist Merkle Tree\n', merkleTree.toString(), 'Root Hash\n', mer
 const claimingAddress = leafNodes[0];
 
 const hexProof = merkleTree.getHexProof(claimingAddress);
-console.log('Merkle Proof for Address\n', hexProof)
+console.log('Merkle Proof for Address\n', hexProof);
 
 console.log(merkleTree.verify(hexProof, claimingAddress, merkleRoot));
 
